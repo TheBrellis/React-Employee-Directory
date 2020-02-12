17a254e10b3d34fff3367705ce7fc10c567242d3
TheBrellis <brianroyalellis@gmail.com>
2020-02-11 19:07:55 -0500

    finished writting API call function for seed data

--- utils/API.js
+++ utils/API.js
@@ -1,14 +1,7 @@
-import React from "react";
-import axios from "axios"
+import axios from 'axios';
 
-const QTY = 100;
-const BASEURL = `randomuser.me/api/?results=${QTY}&nat=us`;
-
-export default class API extends React.Component {
-
-    renders() {
-        returns(
-           
-        )
-    }
+export function GetSeeds(query) {
+    const QTY = 100;
+    const BASEURL = `randomuser.me/api/?results=${QTY}&nat=us`;
+    return axios.get(url)
 }
