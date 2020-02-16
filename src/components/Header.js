0b02727919ad28b7e8e2b3d6e44413b113b7628c
TheBrellis <brianroyalellis@gmail.com>
2020-02-16 10:28:01 -0500

    added basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
-
+import {Jumbotron, Container} from "react-bootstrap"
 export default function Header() {
 
     return (
-        <div>
-            Hello World2
-        </div>
+        <Jumbotron fluid>
+        <Container>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal space of
+            its parent.
+          </p>
+        </Container>
+      </Jumbotron>
     )
-
-
 
 }
--- src/pages/DirectoryContainer.js
+++ src/pages/DirectoryContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from "../components/Header"
 
-
 export default function DirectoryContainer() {
-  return (
-    <Header />
-  );
+    return (
+        <div>
+            <Header />
+        </div>
+
+    );
 }
